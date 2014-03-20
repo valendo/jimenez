941690dc04649778f4a3ff3e57da9a0e2c4a0725
valendo <vothanhtai@hotmail.com>
2014-03-20 22:12:16 +0700

    apply index template

---
+++ wp-content/themes/blankslate/footer.php
@@ -0,0 +1,48 @@
+<!-- footer -->
+<div class="footer">
+  <div class="subfooter">
+    <p> <a href="index.html" class="active" >Home </a> <span>| </span> <a href="about.html" > About </a> <span>| </span> <a href="getaquote.html"> Get a Quote </a> <span>| </span> <a href="insuranceproduct.html" >Insurance Product </a><span class="hide">| </span> <a href="partners.html" >Partners </a> <span>| </span> <a href="blog.html" >Blog </a> <span>| </span> <a href="contact.html" >Contact Us </a> </p>
+    <p>Email Marketing you can trust  |  Subscribe to our newsletter</p>
+    <p class="subscribe">
+      <input class="input" type="text" value="Name.." id="Username"  onfocus="if(this.value == 'Name..') this.value = '';" onblur="if(this.value == '') this.value = 'Name..';"  />
+      <input class="input" type="text" value="Inter your E-mail.." id="Username"  onfocus="if(this.value == 'Inter your E-mail..') this.value = '';" onblur="if(this.value == '') this.value = 'Inter your E-mail..';"  />
+      <input name="" type="button" value="Submit" class="btsubmit">
+    </p>
+    <p>©2014 Jason Jimenez Insurance Agency INC. All rights reserved. </p>
+    <p class="social"> <a id="facebook" href="https://www.facebook.com/jasonjimenezinsuranceagency" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/f.png"></a> <a id="sweeter" href="https://twitter.com/JasonJimenezIns" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/t.png"></a> <a id="in" href="http://www.linkedin.com/pub/jason-jimenez/12/106/202" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/i.png"></a></p>
+  </div>
+</div>
+<!-- box chat with me --> 
+<a class="btlivechat" href="#">Live chat with me</a>
+<div class="live-chat">
+  <div class="block-heading"> <a class="btn-close" href="javascript:;">&nbsp;</a> <strong>Live Chat</strong> </div>
+  <div class="block-content">
+    <div class="box-monster">
+      <figure class="img-monster"><img alt="" src="img/support-icon.png"></figure>
+      <span>Hello! Can I help you choose a insurrane product?</span> </div>
+    <form class="live-chat-form" id="live-chat-form" name="live_chat" method="POST" action="">
+      <div class="erro" style="display:none"></div>
+      <div class="form-group ">
+        <input class="input" type="text" value="Name.." id="Username"  onfocus="if(this.value == 'Name..') this.value = '';" onblur="if(this.value == '') this.value = 'Name..';"  />
+      </div>
+      <div class="erro" style="display:none"></div>
+      <div class="form-group">
+        <input class="input" type="text" value="Inter your E-mail.." id="Username"  onfocus="if(this.value == 'Inter your E-mail..') this.value = '';" onblur="if(this.value == '') this.value = 'Inter your E-mail..';"  />
+      </div>
+      <div class="form-group">
+        <input name="" type="button" class=" btn" value="Star chat">
+      </div>
+    </form>
+    <div class="call">if you have questions? you can call for me <strong>866-835-9301</strong></div>
+  </div>
+</div>
+<p id="back-top"> <a href="#top"><span></span>Back to Top</a> </p>
+<!-- sript ---> 
+<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script> 
+<script src="<?php bloginfo('template_directory'); ?>/js/modernizr.js"></script> 
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/responsivemobilemenu.js"></script> 
+<script src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>
+
+</body>
+
+</html>
---
+++ wp-content/themes/blankslate/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
+<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+<!-- viewport meta to reset iPhone inital scale -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php wp_title( ' | ', true, 'right' ); ?></title>
+<!-- css3-mediaqueries.js for IE8 or older -->
+<!--[if lt IE 9]>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<![endif]-->
+<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+<?php wp_head(); ?>
+</head>
+
+<body>
+<!-- header-->
+<header>
+  <div class="headwrap"> <a href="home.html" class="logo"><img src="<?php bloginfo('template_directory'); ?>/img/logo.png"></a>
+    <p class="call">QUESTIONS? CALL <span class="numb"> 866-835-9301</span></p>
+  </div>
+</header>
+<!-- navi-->
+<div class="navi">
+  <div class="rmm">
+    <ul>
+      <li><a href='index.html' class="active">Home</a></li>
+      <li><a href='about.html'>About </a></li>
+      <li><a href='getaquote.html'> get quote </a></li>
+      <li><a href='insurance.html'>insurance product</a></li>
+      <li><a href='partners.html'>Partners </a></li>
+      <li><a href='#sitemap'>blog</a></li>
+      <li><a href='contact.html'>contact us</a></li>
+    </ul>
+  </div>
+</div>
